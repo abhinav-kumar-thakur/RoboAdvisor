e45778e8688bc3684e349621444770bf069e2832
nayanab <nayanabm.94@gmail.com>
2016-10-13 17:26:05 +0530

    nayana/ravi added newgroup and news

--- app/databaseApi/news.py
+++ app/databaseApi/news.py
@@ -1,11 +1,9 @@
-from app.models import News
+from app.models import News, NewsGroup
+from datetime import datetime
 
 
 class NewsApi():
-    def addHeadline(self):
-        news = News()
+    def addHeadline(self, headline, url, sentiment):
+        for group in NewsGroup.objects.all():
+            news = News(timeStamp=datetime.now(), groupId=group, headline=headline, url=url, sentiment=sentiment)
         news.save()
-
-
-if __name__ == '__main__':
-    NewsApi.addHeadline();
