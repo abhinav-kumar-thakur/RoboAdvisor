9d3acfc2f2717a45636910129cab3d49ee031404
Vishal Sharma <vishalsh@thoughtworks.com>
2016-10-19 11:34:27 +0530

    [Vishal] #000 Adding npm redux dev tools

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -3,15 +3,17 @@ import { render } from 'react-dom';
 import thunkMiddleware from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import roboAdvisorApp from './AppReducer';
 import RoboAdvisorAppRouter from './AppRouter';
 
 const store = createStore(
   roboAdvisorApp,
-  applyMiddleware(
-    thunkMiddleware // lets us dispatch() functions
-  )
+  composeWithDevTools(
+    applyMiddleware(
+      thunkMiddleware // lets us dispatch() functions
+    ))
 );
 
 render(
