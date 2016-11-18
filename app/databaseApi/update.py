12a29d162f6c16ac7ebc4875c16f062453801001
nayanab <nayanabm.94@gmail.com>
2016-11-18 10:58:35 +0530

    Nayana temp

--- app/databaseApi/update.py
+++ app/databaseApi/update.py
@@ -18,8 +18,8 @@ yesterday = (datetime.today() - timedelta(1)).date()
 lastUpdateDate = status.lastUpdateDate.date()
 startUpdateDate = lastUpdateDate + timedelta(1)
 if yesterday != lastUpdateDate:
-    AssetDataApi().addDetails(prediction=0.0, errorMargin=0.1, neteffect=0.0, startDate=str(startUpdateDate),
-                              endDate=str(yesterday),
+    AssetDataApi().addDetails(prediction=0.0, errorMargin=0.1, neteffect=0.0, startDate="2016-11-17",
+                              endDate="2016-11-17",
                               arimaeffect=0.0)
     status.lastUpdateDate = yesterday
     status.save()
