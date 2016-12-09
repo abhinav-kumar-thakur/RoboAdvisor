4148473d844d54fb7d3ef6b4916c73bbefafa6ae
nayanab <nayanabm.94@gmail.com>
2016-12-09 09:49:26 +0530

    Nayana revert

--- app/databaseApi/assetData.py
+++ app/databaseApi/assetData.py
@@ -6,25 +6,21 @@ from collections import OrderedDict
 
 
 class AssetDataApi():
-    def addDetails(self, prediction, errorMargin, neteffect, startDate, endDate, arimaeffect, historicalData=None,
-                   endDateData=None):
+    def addDetails(self, prediction, errorMargin, neteffect, startDate, endDate, arimaeffect):
         date_format = "%Y-%m-%d"
         for asset in Asset.objects.all():
             symbol = asset.symbol
             assetInfo = Share(symbol)
-            historicalData.append(assetInfo.get_historical(startDate, endDate))
-
-        for data in historicalData:
-            if data['Date'] == endDate:
-                endDateData.append(data)
-            if len(endDateData) != 10:
-                raise Exception
-            else:
-                historicalData.reverse()
-                for dailyData in historicalData:
-                    data = AssetData(asset=asset, errorMargin=errorMargin, prediction=prediction,
-                                     price=dailyData['Close'],
-                                     timestamp=datetime.strptime(dailyData['Date'], date_format), neteffect=neteffect,
-                                     arimaeffect=arimaeffect)
-
-                data.save()
+        while True:
+            try:
+                historicalData = assetInfo.get_historical(startDate, endDate)
+                break
+            except:
+                pass
+            historicalData.reverse()
+            for dailyData in historicalData:
+                data = AssetData(asset=asset, errorMargin=errorMargin, prediction=prediction,
+                                 price=dailyData['Close'],
+                                 timestamp=datetime.strptime(dailyData['Date'], date_format), neteffect=neteffect,
+                                 arimaeffect=arimaeffect)
+            data.save()
